f4e6c309eac0ddff62c977ec8f8cac7f10d3ba17
LuisDAguilarA <luiscosconta@gmail.com>
2015-05-11 16:14:14 -0600

    Actualizacion de clases Exell

--- Servidor/src/Logica/ExcellInterface.java
+++ Servidor/src/Logica/ExcellInterface.java
@@ -1,18 +1,25 @@
 
 package Logica;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author root
  * Interface para el correcto manejo de la clase Exell
  */
     public abstract interface  ExcellInterface{
-    
-        public void cargarTikets();
-        public void cargarTiketsPendientes();
-        public void cargarTiketsRojos();
-        public void cargarTiketsVerdes();
-        public void cargarTiketsAmarillos();
-        public void cargarGuardarCambios();
+        /**
+         * Metodo para setear el path del Archivo a leer
+         * @param path Proveniente del JChoser
+         */
+        public void setPathDeArchivo(String path);
+        
+        public ArrayList<Tickets>  cargarTiketsDeArchivo();
+        public ArrayList<Tickets>  cargarTiketsPendientesDeArchivo();
+        public ArrayList<Tickets>  cargarTiketsRojosDeArchivo();
+        public ArrayList<Tickets>  cargarTiketsVerdesDeArchivo();
+        public ArrayList<Tickets>  cargarTiketsAmarillosDeArchivo();
+        public void cargarGuardarCambiosEnArchivo();
     
 }
--- Servidor/src/Logica/Exell.java
+++ Servidor/src/Logica/Exell.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Logica;
 
+import java.util.ArrayList;
+
+
+
+
 /**
- *
  * @author root
+ * Clase que tendra la responsabilidad de ejecutar la apertura y guardado de archivos .xls
+ * Para el cargado de Tikets nuevos al sistema y el guardado de los cambios es ellos.
  */
-public class Exell {
+public class Exell implements ExcellInterface{
+    /**
+    * Atributos usados para un mejor desempeño de Exell.class
+    */
+    private int pagina;
+
+    @Override
+    public void setPathDeArchivo(String path) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Tickets> cargarTiketsDeArchivo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Tickets> cargarTiketsPendientesDeArchivo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Tickets> cargarTiketsRojosDeArchivo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Tickets> cargarTiketsVerdesDeArchivo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Tickets> cargarTiketsAmarillosDeArchivo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void cargarGuardarCambiosEnArchivo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
     
 }
